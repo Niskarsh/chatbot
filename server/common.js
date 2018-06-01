51cd6e624c49e07269d93d505276746a783851f3
Niskarsh Kumar <niskarshk@gmail.com>
2018-06-01 21:24:14 +0530

    Doing it

--- server/common.js
+++ server/common.js
@@ -3,7 +3,7 @@ const handlebars = require("hbs");
 const bodyparser = require("bodyParser");
 const app = express();
 app.set("view engine", handlebars);
-app.use(express.static(__dirname+"/../public"));
+app.use(express.static(__dirname+"/public"));
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({
     extended: true
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import { get, listen } from "./common.js";
 const port = process.env.PORT || 3000;
 
 get("/",async (req, res)=>{
-
+    res.render("home.hbs");
 });
 
 listen(port,console.log(`Started listening on port ${port}`));
